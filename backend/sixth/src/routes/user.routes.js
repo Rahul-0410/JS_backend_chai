6c70f231f948cf64f3cb95de428371f0e3177a46
Rahul-0410 <rgs786999@gmail.com>
2025-06-13 11:40:23 +0530

    added-routes

--- backend/sixth/src/routes/user.routes.js
+++ backend/sixth/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from '../controllers/user.controller.js'
+import {changeCurrentPassword, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken,
+    registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage} from '../controllers/user.controller.js'
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router();
@@ -22,6 +23,22 @@ router.route("/login").post(loginUser);
 //secrued router
 
 router.route("/logout").post(verifyJWT, logoutUser);
+
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT,getSelection);
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar);
+
+router.route("/coverIMage").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage);
+
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile);
+
+router.route("/history").get(verifyJWT,getWatchHistory);
+
 
 export default router;
