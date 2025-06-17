192ea2821f83a4bbb7764a3a609a4621a3132135
Rahul-0410 <rgs786999@gmail.com>
2025-06-17 20:50:11 +0530

    added files for dashboard

--- backend/sixth/src/app.js
+++ backend/sixth/src/app.js
@@ -21,6 +21,7 @@ import commentRouter from './routes/comment.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import likeRouter from './routes/like.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
+import dashboardRouter from './routes/dashboard.routes.js'
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/videos",videoRouter);
 app.use ("/api/v1/subscriptions",subscriptionRouter)
@@ -28,5 +29,6 @@ app.use ("/api/v1/comment",commentRouter)
 app.use ("/api/v1/tweets",tweetRouter)
 app.use ("/api/v1/likes",likeRouter)
 app.use ("/api/v1/playlist",playlistRouter)
+app.use ("/api/v1/dashboard",dashboardRouter)
 
 export{app};
